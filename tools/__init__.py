2aa941f9e73ab7080b9e2ea06ab9de73bfb537a9
keishi.ishihara <keishi.ishihara17@gmail.com>
2024-12-07 17:26:42 +0900

    Remove custom env registrations

--- tools/__init__.py
+++
@@ -1,9 +0,0 @@
-from gymnasium import register
-
-# Equivalent settings to the legacy "CartPole-v0", which is deprecated in gymnasium.
-register(
-    id="CartPole-v3",
-    entry_point="gymnasium.envs.classic_control:CartPoleEnv",
-    max_episode_steps=200,
-    reward_threshold=195.0,
-)
